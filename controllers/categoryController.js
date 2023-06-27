cc10f85f1e2b28b2e3a106d4e04498b7d46761f0
jorge niglia <jorge.niglia@gmail.com>
2023-06-26 22:10:05 -0300

    find3

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -33,7 +33,7 @@ const categoryControllers = {
 		}
 	},
 	allCategories: async (req, res) => {
-		const categories = await Category.find({ categories });
+		const { categories } = await Category.find();
 		res.status(200).json({ categories });
 	},
 	categoryById: async (req, res) => {
