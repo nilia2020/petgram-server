1086db246f4b1aa6c59b05e97658774e76c8f857
jorge niglia <jorge.niglia@gmail.com>
2023-07-12 11:13:46 -0300

    photos by categories

--- routes/categories.js
+++ routes/categories.js
@@ -2,48 +2,28 @@ var express = require("express");
 var router = express.Router();
 const controller = require("../controllers/categoryController");
 const { check } = require("express-validator");
-const {
-	validateCategoryId,
-} = require("../middlewares/validations");
+const { validateCategoryId } = require("../middlewares/validations");
 
 /* Crear nueva categoria */
 router.post(
-	"/new",
-	[
-		check("name")
-			.not()
-			.isEmpty()
-			.withMessage("Debes escribir un nombre"),
-	],
-	controller.newCategory
+    "/new",
+    [check("name").not().isEmpty().withMessage("Debes escribir un nombre")],
+    controller.newCategory
 );
 /* Ver todas las categorias */
 router.get("/seeall", controller.allCategories);
 
 /* Ver una categoria*/
-router.get(
-	"/see/:id",
-	validateCategoryId,
-	controller.categoryById
-);
+router.get("/see/:id", validateCategoryId, controller.categoryById);
 module.exports = router;
 
 /* Editar categoria */
 router.put(
-	"/edit/:id",
-	validateCategoryId,
-	[
-		check("name")
-			.not()
-			.isEmpty()
-			.withMessage("El campo name esta vacio"),
-	],
-	controller.editCategory
+    "/edit/:id",
+    validateCategoryId,
+    [check("name").not().isEmpty().withMessage("El campo name esta vacio")],
+    controller.editCategory
 );
 
 /* Borrar categoria*/
-router.delete(
-	"/delete/:id",
-	validateCategoryId,
-	controller.deleteCategory
-);
+router.delete("/delete/:id", validateCategoryId, controller.deleteCategory);
--- routes/photos.js
+++ routes/photos.js
@@ -2,44 +2,32 @@ var express = require("express");
 var router = express.Router();
 const controller = require("../controllers/photoController");
 const { check } = require("express-validator");
-const {
-	validatePhotoId,
-} = require("../middlewares/validations");
+const { validatePhotoId } = require("../middlewares/validations");
 
 /* Crear nueva foto */
 router.post(
-	"/new",
-	[
-		check("src")
-			.not()
-			.isEmpty()
-			.withMessage("Debes escribir un link a una foto"),
-	],
-	controller.newPhoto
+    "/new",
+    [check("src").not().isEmpty().withMessage("Debes escribir un link a una foto")],
+    controller.newPhoto
 );
 /* Ver todas las fotos */
 router.get("/seeall", controller.allPhotos);
 
 /* Ver una foto*/
 router.get("/see/:id", validatePhotoId, controller.PhotosById);
-module.exports = router;
+
+/* Filtrar por categoria */
+router.get("/see/:categoryId", validatePhotoId, controller.PhotosByCategoryId);
 
 /* Editar foto */
 router.put(
-	"/edit/:id",
-	validatePhotoId,
-	[
-		check("name")
-			.not()
-			.isEmpty()
-			.withMessage("El campo name esta vacio"),
-	],
-	controller.editPhoto
+    "/edit/:id",
+    validatePhotoId,
+    [check("name").not().isEmpty().withMessage("El campo name esta vacio")],
+    controller.editPhoto
 );
 
 /* Borrar foto*/
-router.delete(
-	"/delete/:id",
-	validatePhotoId,
-	controller.deletePhoto
-);
+router.delete("/delete/:id", validatePhotoId, controller.deletePhoto);
+
+module.exports = router;
